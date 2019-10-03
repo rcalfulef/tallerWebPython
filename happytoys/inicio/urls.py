e214f84b71ed75befde123e80b1702c62b9810f9
Andres <rcalfulef@gmail.com>
2019-10-03 10:31:49 -0300

    cambio de estilo, agregado boton de inicio sesion, barra de filtrado

--- happytoys/inicio/urls.py
+++ happytoys/inicio/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^descripcion/$',descripcion),
     url(r'^login/$',login),
     url(r'^login/logueado/$',vista_logueado),
+    url(r'^juguetes/$',juguetes),
     url(r'^buscador/$',buscador),
-     
+    url(r'^logout/$',logout,name ='logout')
 ]
--- happytoys/inicio/views.py
+++ happytoys/inicio/views.py
@@ -23,6 +23,20 @@ def buscador(request):
             context = {'resultado':resultado}
     return render(request,"buscador.html",context)
 
+def juguetes(request):
+    context = {}
+    if request.method == "POST":
+        if "buscador" in request.POST.keys():
+            resultado = Juguete.objects.filter(nombre__contains = request.POST['buscador'])
+            context = {'resultado': resultado}
+    
+    else: # si ingremos a la URL y no realizamos ninguna busqueda
+        resultado = Juguete.objects.all()
+        context = {'resultado': resultado}
+
+    return render(request,"juguetes.html",context)
+
+
 def login(request):
     if request.method == "POST":
         if ("user" in request.POST.keys()) and ("password" in request.POST.keys()):
@@ -31,7 +45,7 @@ def login(request):
             print(user)
             if user is not None and user.is_active:
                 auth.login(request,user)
-                return redirect("/login/logueado/")
+                return redirect("/")
             else:
                 contexto = {"error" : "error"}
                 return render(request,"login.html",contexto)
@@ -41,10 +55,15 @@ def login(request):
             return render(request,"login.html",contexto)
     return render(request,"login.html")
 
+def logout(request):
+    auth.logout(request)
+    return redirect("/")
+
 @login_required(login_url='/login')
 def vista_logueado(request):
     if request.method == 'POST':
         if ("cerrar" in request.POST.keys()):
             auth.logout(request)
-            return redirect("/login/")
+            return redirect("/")
     return render(request,"logueado.html")
+
